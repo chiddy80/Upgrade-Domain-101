d44b03277cf00613706082ea1305b114247aadf8
chiddy80 <rashidkamote95@gmail.com>
2025-11-27 16:12:01 +0300

    Update server.js

--- server.js
+++ server.js
@@ -2,13 +2,31 @@ const express = require('express');
 const WebSocket = require('ws');
 
 const app = express();
-const server = app.listen(process.env.PORT || 3000);
-
-const wss = new WebSocket.Server({ server });
-
-wss.on('connection', (ws) => {
-    console.log('Client connected');
-    ws.send('Welcome to North Africa WebSocket!');
+const PORT = process.env.PORT || 3000;
+const server = app.listen(PORT, '0.0.0.0', () => {
+  console.log(`WebSocket server running on port ${PORT}`);
 });
 
-console.log('WebSocket server running on port 3000');
+const wss = new WebSocket.Server({ 
+  server,
+  path: '/'
+});
+
+// Basic HTTP route
+app.get('/', (req, res) => {
+  res.send('WebSocket server is running!');
+});
+
+// WebSocket connection
+wss.on('connection', (ws, req) => {
+  console.log('Client connected from:', req.socket.remoteAddress);
+  ws.send('Welcome to North Africa WebSocket!');
+  
+  ws.on('message', (message) => {
+    console.log('Received:', message.toString());
+  });
+  
+  ws.on('close', () => {
+    console.log('Client disconnected');
+  });
+});
